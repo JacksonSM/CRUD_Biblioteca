6d8fe8c05336d4e8d30fec624c3c02e4c496f0e5
JacksonSM <jacksons.m2018@gmail.com>
2022-10-24 10:56:39 -0300

    Criado validação para a classe Editora.

--- src/Biblioteca.Domain/Entities/Editora.cs
+++ src/Biblioteca.Domain/Entities/Editora.cs
@@ -1,9 +1,28 @@
-﻿namespace Biblioteca.Domain.Entities;
+﻿using Biblioteca.Domain.Entities.Validators;
+
+namespace Biblioteca.Domain.Entities;
 public class Editora : BaseEntity
 {
-    public string RazaoSocial { get; set; }
-    public string Email { get; set; }
-    public string Telefone { get; set; }
+    public string RazaoSocial { get; private set; }
+    public string Email { get; private set; }
+    public string Telefone { get; private set; }
+    public bool IsValid => Validate();
 
-    public List<Livro> Livros { get; set; }
+    public List<Livro> Livros { get; private set; }
+
+    public Editora(string razaoSocial, string email, string telefone)
+    {
+        RazaoSocial = razaoSocial;
+        Email = email;
+        Telefone = telefone;
+    }
+
+    private bool Validate()
+    {
+        return !string.IsNullOrEmpty(RazaoSocial)
+                    && FieldValidator.IsValidEmail(Email)
+                    && FieldValidator.IsValidTelefone(Telefone);
+    }
+
+
 }
---
+++ src/Biblioteca.Domain/Entities/Validators/FieldValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Biblioteca.Domain.Entities.Validators;
+public static class FieldValidator
+{
+    public static bool IsValidEmail(string email)
+    {
+        try
+        {
+            var addr = new System.Net.Mail.MailAddress(email);
+            return addr.Address == email;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    public static bool IsValidTelefone(string telefone)
+    {
+        string padraoTelefone = "[0-9]{2} [1-9]{1} [0-9]{4}-[0-9]{4}";
+        return Regex.IsMatch(telefone, padraoTelefone);
+    }
+}
